dc81b770f55422ed8a51e1ed8b46df112f2e5799
Garam Jang <garamj88@gmail.com>
2022-04-13 13:07:01 -0400

    create movie list form

---
+++ myapp/movie_list/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+class MovieListForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    genre = StringField('Genre', validators=[DataRequired()])
+    submit = SubmitField('Post')
