efa9c3587f4ffe1db9985a8d6ff5abea91dd13c4
Garam Jang <garamj88@gmail.com>
2022-04-13 00:57:23 -0400

    add skeleton

--- myapp/models.py
+++ myapp/models.py
@@ -31,3 +31,19 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Username {self.username}"
 
+class BlogPost(db.Model):
+    __tablename__ = 'blog_postsd'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id = user_id
+    
+    def __repr__(self):
+        return f"Post ID: {self.id} -- Date: {self.date} --- Title: {self.Title}"
