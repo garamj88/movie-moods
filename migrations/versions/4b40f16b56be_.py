7efc392d72fddf7c6ba0fa3d99d1e1489d080e47
Garam Jang <garamj88@gmail.com>
2022-04-14 13:17:46 -0400

    deploy test

---
+++ migrations/versions/4b40f16b56be_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4b40f16b56be
+Revises: 13d02d996199
+Create Date: 2022-04-14 13:11:39.328434
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4b40f16b56be'
+down_revision = '13d02d996199'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
