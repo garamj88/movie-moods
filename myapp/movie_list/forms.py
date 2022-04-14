57c6c281fc9a518f73e6cb97795d9a7a7eb214a6
Garam Jang <garamj88@gmail.com>
2022-04-14 13:01:30 -0400

    update db

--- myapp/movie_list/forms.py
+++ myapp/movie_list/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, TextAreaField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 class MovieListForm(FlaskForm):
